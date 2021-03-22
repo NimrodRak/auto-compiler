5070112167b90da811b483016db490f2f6808b75
Nimrod Rak <nimrod.rak@mail.huji.ac.il>
2021-03-22 14:38:38 +0200

    added .bat and .config files

---
+++ AutoCompiler.bat
@@ -0,0 +1,71 @@
+@echo off
+setlocal EnableDelayedExpansion
+choice /c yn /n /m "Do you wish to recompile your pdfs? (y/n)"
+:: if the answer is not yes (Y), jump to end
+if %ERRORLEVEL%==2 goto END
+:: Check if Uni path environment variable exists
+if not defined UNI (
+	choice /c yn /n /m "UNI environment variable undefined, do you wish to manually input a path? (y/n)"
+	:: If the user doesn't want to manually folder, jump to end
+	if %ERRORLEVEL%==2 goto END
+	set /p UNI="Enter uni folder path: "
+)
+cd %UNI%
+:: Open configurations file in Uni path to locate wanted .lyx files
+for /f "tokens=1* delims==" %%i in (./compilation.config) do (
+	:: Check if to parse enabled propert
+	if %%i==enabled (
+		:: if config says not to run it
+		if not %%j==true (
+			echo Enabled property false. Exiting AutoCompiler...
+			goto end
+		)
+	) else (
+	:: Check if to parse engine property
+	if %%i==engine (
+		if %%j==xelatex set engine=pdf4
+		if %%j==pdflatex set engine=pdf2
+		:: If no valid engine was found, exit the program
+		if not defined engine (
+			echo Engine property invalid. Exiting AutoCompiler...
+			goto end
+		)
+		echo Using %%j...
+	) else (
+	if %%i==conflicts (
+		if %%j==weak (
+			for /f  %%l in ('git rev-list --left-only --count origin/master...master') do if not %%l==0 (
+				echo Unmerged commits from remote repository available. Exiting AutoCompiler...
+				goto end
+			)
+		)
+	) else (
+		:: Declate the current folder
+		set folder=%%i
+		echo !folder!:
+		:: Go over each file in the folder
+		for %%f in (.\"!folder!"\*.lyx) do (
+			:: Check if it's really a ./lyx file
+			if "%%~xf"==".lyx" (
+				set file=%%f
+				:: Derive the name of the pdf file
+				set pdf=!file:~0,-3!pdf
+				:: Check if the matching pdf exists
+				if exist !pdf! (
+					echo|set /p=Deleting !pdf!...
+					:: Delete the previous pdf if it exists
+					del "!pdf!"
+					echo. Done.
+				)
+				echo|set /p=Compiling !file!...
+				:: Export the .lyx file using xelatex
+				lyx --export !engine! "!file!"
+				echo. Done.
+			)
+		)
+	)
+		)
+	)
+)
+:END
+Pause
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # auto-compiler
+
+Compiles automatically all `.lyx` files in folders of `%UNI%\compilation.config`
+
+## Usage
+ - Set your `UNI` environment variable to wherever your `.lyx` files are located.
+ - Clone this repository.
+ - Copy the `compilation.config` file into `%UNI%` and edit it to your needs.
+ - Create a shortcut to `AutoCompiler.bat`.
+ - Open `Run` and enter `shell:startup` and place the shortcut in the folder.
+
+### Configuration file
+A valid configuration file must include the following:
+ - an `enabled` property: `true` if you wish the program to run on startup and `false` otherwise.
+ - an `engine` property: `pdflatex` or `xelatex` according to your needs. **Only `pdflatex` and `xelatex` are currently supported**
+ - a `conflicts` property: `weak` if you wish the compilation to stop when it meets a conflict with the remote repository associated with the `%UNI%` folder. `force` if the compilation should continue even when the remote is ahead of the local repository. `disabled` if git is not initialized in `%UNI%`.
+ - All the subfolders of `%UNI%` that you wish to have they LyX files in them compiled.
+## Requirements
+Windows 10
+
+## Notes
+This program will compile your LyX files using `xelatex`, if your LyX files are compiled using `pdflatex`, change the `engine` variable in `compilation.config` to `pdflatex`. 
+
+If you wish to disable the program, change the `enabled` variable to `false`.
